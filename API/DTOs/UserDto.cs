58079e0cc4d18a881b718d53d349145aaf20a461
Timo Keränen <timo.keranen@lisker.fi>
2024-01-02 14:27:52 +0200

    End of  section 12

---
+++ API/DTOs/UserDto.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+  public class UserDto
+  {
+    public string DisplayName { get; set; }
+    public string Token { get; set; }
+    public string Image { get; set; }
+    public string Username { get; set; }
+  }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,9 @@
 using API.Extensions;
 using API.Middleware;
+using Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -8,8 +12,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Ei ole väliä missä järjestyksessä palvelut lisätään.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt => {
+  var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+  opt.Filters.Add(new AuthorizeFilter(policy));
+});
 builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -25,6 +33,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseCors("CorsPolicy");
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
@@ -35,8 +44,9 @@ var services = scope.ServiceProvider;
 try
 {
   var context = services.GetRequiredService<DataContext>();
+  var userManager = services.GetRequiredService<UserManager<AppUser>>();
   await context.Database.MigrateAsync();
-  await Seed.SeedData(context);
+  await Seed.SeedData(context, userManager);
 }
 catch (Exception ex)
 {
